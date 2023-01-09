86513e26b3c332e66b7d17217efab94546064efe
artem <artemfr@hotmail.com>
2023-01-09 12:53:47 +0100

    optional chaining

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-function greet(name: string | null | undefined)  {
-    if (name) 
-    console.log(name.toUpperCase());
-    else
-    console.log('Hola!');
-    
-    
-}
+type Customer = {
+    birthday?: Date
+};
 
-greet(undefined);
+function getCustomer(id: number): Customer | null {
+    return id === 0 ? null : {birthday: new Date() };
+}
+let customer = getCustomer(1);
+// Optional property access opearator
+console.log(customer?.birthday?.getFullYear());
+
+let log: any = null;
+log?.('a');
