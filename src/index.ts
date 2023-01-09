e0093289d3e3247b4883928d17a8c76274eba8bb
artem <artemfr@hotmail.com>
2023-01-09 12:28:09 +0100

    type alias

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-let employee: {
+type Employee = {
     readonly id: number,
     name: string,
     retire: (date: Date) => void
-} = { 
+} 
+let employee: Employee = {
     id: 1, 
     name: 'Mosh',
     retire: (date: Date) => {
     console.log(date);
     }
-};
+}
+
